7f8b8b5717f02b1179fc4acd9a90070f56d0b9a8
robertkeithrippetoe <rkrippetoe@gmail.com>
2018-12-03 14:12:13 -0700

    Add S3 API

---
+++ pkg/apis/cache/v1alpha1/s3_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// S3Spec defines the desired state of S3
+type S3Spec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// S3Status defines the observed state of S3
+type S3Status struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// S3 is the Schema for the s3s API
+// +k8s:openapi-gen=true
+type S3 struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   S3Spec   `json:"spec,omitempty"`
+	Status S3Status `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// S3List contains a list of S3
+type S3List struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []S3 `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&S3{}, &S3List{})
+}
